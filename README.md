c27ac24e28ab535c906abb90ee37d4d6904bb16f
Michael Chen <hua.michael.chen@gmail.com>
2019-11-22 02:45:38 -0500

    done

--- README.md
+++ README.md
@@ -1,19 +1,40 @@
 ### Quick Start
-
 1. `docker-compose up --build -d`
 2. visit http://localhost:5000
+3. go to the register page to create a random user
+4. follow the buttons
+5. `docker-compose down` to destroy everything
+
+NOTE: you may need to run `sudo docker-compose up --build -d` again if you want to poke around due to volume mount default
+to `root`. Probably can be solved using
+* user_id mapping
+* or use [rootless docker](https://www.katacoda.com/courses/docker/rootless)
+
+
+### How to use
+* visit http://localhost:8481 to see what data has been created in MongoDB
+* Follow the ugly buttons (sorry about that...)
+* "Generate Webhook Endpoint for User" button to generate a webhook url
+  * which will lead you to a page with two options, "Kick off WebHook" or "Observe WebHook"
+  * Copy that GUID number and find it inside *MongoDB Express GUI* (aka. http://localhost:8481), you won't see 'Payload'
+  * Now click on "Kick off WebHook" button, then go back to *MongoDB Express GUI* and click on the "find" button there 
+  again to see 'Payload' been added. (However, in this case, there is not really any payload...)
 
 ### Requirements:
-
-
-* [ ] Support multiple unique endpoints to enable simultaneous testing of several webhooks at once.
+* [x] Support multiple unique endpoints to enable simultaneous testing of several webhooks at once.
 * [x] Be a stateless service with a backing datastore for persistence.
 * [x] Have a functional health check.
 * [x] Execute within a Docker container.
 
 ### ToDos:
 
-1. Add data to MongoDB
-2. Add a stub trigger event
-3. Add concurrency
-4. Have some GUI..
+* [x] Add data to MongoDB
+* [x] Add a stub trigger event
+* ~~Add concurrency~~
+* [x] Have some GUI.. (well, somewhat a GUI...)
+
+### Further improvements:
+* [ ] Add JWT
+* [ ] Better frontend for usability or at least bare-minimal of ease of navigation
+* [ ] Logout...
+* [ ] Test with a true WebHook service from other applications, such as Google Hangout, GitHub etc.
--- app.py
+++ app.py
@@ -1,25 +1,24 @@
-from flask import Flask, request, jsonify, redirect, url_for
-from urllib.parse import unquote_plus
+from flask import Flask, request, jsonify, redirect, url_for, session, render_template
+from flask_pymongo import PyMongo
 from pymongo import MongoClient
-from flask_jwt_extended import JWTManager
 from healthcheck import HealthCheck, EnvironmentDump
 
-import json
-import re
-import uuid
 import datetime
+import bcrypt
+import uuid
 import os
+import pymongo
 
 # App configs
 app = Flask(__name__)
-app.config['MONGO_URI'] = os.environ.get('DB')
-app.config['JWT_SECRET_KEY'] = os.environ.get('SECRET')
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
-
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
 
 # Initialization
-connection = MongoClient(os.environ.get('DB'))
-jwt = JWTManager(app)
+connection = MongoClient(os.environ.get('MONGO_URI'))
+database = connection[os.environ.get('MONGO_DBNAME')]
+collection = database[os.environ.get('MONGO_DBNAME')]
+mongo = PyMongo(app)
 health = HealthCheck(app, "/healthcheck")
 envdump = EnvironmentDump(app, "/environment")
 
@@ -27,54 +26,117 @@ envdump = EnvironmentDump(app, "/environment")
 @app.route('/', methods=['GET'])
 def index():
     """
-    Go to localhost:5000 and redirect to localhost:/svc
+
+    :return: render the index.html page.
     """
-    return redirect(url_for('svc'))
+    if 'username' in session:
+        #return 'You are logged in as ' + session['username'] + ' and your access token is: ' + generate_access_token()
+        return render_template('dashboard.html')
+    return render_template('index.html')
 
 
-@app.route('/svc', methods=['GET'])
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    """
+
+    :return: render register.html page. However, if after a POST request to register a user that's already existed, it
+        will show a warning on the page.
+    """
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name': request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name': request.form['username'], 'password': hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+
+        return 'That username already exists!'
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    """
+
+    :return: redirect to index.html if login successful otherwise return a warning on the page.
+    """
+    users = mongo.db.users
+    login_user = users.find_one({'name': request.form['username']})
+
+    if login_user:
+        if bcrypt.checkpw(request.form['pass'].encode('utf-8'),
+                          bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())):
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+
+    return 'Invalid username/password combination'
+
+
+# def generate_access_token():
+#     return create_access_token(identity=session['username'])
+
+
+@app.route('/svc', methods=['POST'])
 def svc():
     """
-    Generate a GUID
+
+    :return: Renders the webhook.html page
     """
-    return jsonify(webhook_endpoint="Here is your WebHook Location: http://localhost:5000/svc/" + generate_guid()), 200
+    guid = generate_guid()
+    collection.insert_one({"guid": guid,
+                           "time": datetime.datetime.now(),
+                           "user": session['username']})
+    return render_template("webhook.html", guid=guid)
 
 
 def generate_guid():
+    """
+
+    :return: a GUID like string
+    """
     return str(uuid.uuid1())
 
 
-@app.route('/svc/<string:guid>', methods=['GET', 'POST'])
-def print_test(guid: str):
+@app.route('/mongodb_status')
+def mongodb_status():
     """
-    Send a POST request to localhost:5000/api/print with a JSON body with a "p" key
-    to print that message in the server console.
+
+    :return: mongodb server information in json format
     """
-    payload = parse_request(request)
-    print(payload)
-    return (payload, 200, None)
+    return jsonify(connection.server_info()), 200
+
+
+@app.route('/svc/guid/<string:guid>', methods=['GET', 'POST'])
+def trigger_test_event(guid: str):
+    """
+
+    :param guid: the GUID string to be passed in
+    :return:
+        if HTTP 'GET' is called:
+            return the latest record in the mongodb collection. This is to simulate latest updated Webhook event
+        if HTTP 'POST' is called:
+            return the updated record in raw string format. This is to simulate a Webhook event had happened. In this case
+            it updated a record in MongoDB.
+    """
+    if request.method == 'GET':
+        return str(collection.find().sort('$natural', pymongo.DESCENDING).limit(-1).next())
+    if request.method == 'POST':
+        payload = parse_request(request)
+        return str(collection.find_one_and_update({'guid': guid}, {"$set": {"payload": payload}}))
 
 
 def parse_request(req):
     """
-    Parses application/json request body data into a Python dictionary
+
+    :param req: an HTTP request
+    :return: Any response from the HTTP request regardless of the format
     """
-    if req.headers['Content-Type'] == 'text/plain':
-        return "Text Message: " + request.data
-
-    elif req.headers['Content-Type'] == 'application/json':
-        return "JSON Message: " + json.dumps(request.json)
-
-    elif req.headers['Content-Type'] == 'application/octet-stream':
-        f = open('./binary', 'wb')
-        f.write(request.data)
-        f.close()
-        return "Binary message written!"
-    else:
-        return "415 Unsupported Media Type ;)"
-
-    return payload
+    return req.get_data()
 
 
 if __name__ == '__main__':
+    app.secret_key = os.environ.get('SECRET')
     app.run(debug=True, host='0.0.0.0', use_reloader=True)
